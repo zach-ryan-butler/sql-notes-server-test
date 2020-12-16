6509e499db2dca6367e74d0b0b68780dc8c1c0f9
Zach Butler <zach.standley93@gmail.com>
2020-12-15 17:21:09 -0800

    add a new test to get all notes

--- __test__/notes.test.js
+++ __test__/notes.test.js
@@ -2,6 +2,7 @@ const app = require('../lib/app');
 const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
+const Note = require('../lib/models/Note');
 
 describe('notes routes', () => {
   beforeEach(() => {
@@ -25,6 +26,29 @@ describe('notes routes', () => {
       title: 'I a note title',
       body: 'la la la im a happy note'
     });
+  });
+
+  it('can get all notes using GET', async() => {
+    const notes = await Promise.all([
+      {
+        title: 'First Note',
+        Body: 'Brand new baby note'
+      },
+      {
+        title: 'Second Note',
+        Body: 'I am a note'
+      },
+      {
+        title: 'Third Note',
+        Body: 'Being a note is fun'
+      }
+    ].map(note => Note.insert(note)));
+
+    const response = await request(app)
+      .get('/api/v1/notes');
+
+    expect(response.body).toEqual(expect.arrayContaining(notes));
+    expect(response.body).toHaveLength(notes.length);
   });
 });
 
